e6c37603df26279bc461140c7ce4f9c4c7824b90
kestes05 <kestes05@gmail.com>
2019-02-07 13:57:43 -0500

    Bug fix for app.js

--- DiceGameProject/app.js
+++ DiceGameProject/app.js
@@ -22,6 +22,7 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         var diceDOM = document.querySelector('.dice')
         diceDOM.style.display = 'block';
         diceDOM.src = 'dice-' + dice + '.png';
+        document.getElementById('footer').textContent = 'Player ' + (activePlayer + 1) + ' is' + ' currently' + ' rolling...';
 
 
         // 3. Update the round score IF the rolled number was NOT a 1.
@@ -89,6 +90,7 @@ function init() {
     gamePlaying = true;
 
     document.querySelector('.dice').style.display = 'none';
+    document.getElementById('footer').textContent = 'Time ' + 'to ' + 'play ' + 'a ' + 'game!';
 
     document.getElementById('score-0').textContent = '0';
     document.getElementById('score-1').textContent = '0';
