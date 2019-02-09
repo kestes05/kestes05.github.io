6bcf150a07085f1bd9afdc6117611a581368f4d2
kestes05 <kestes05@gmail.com>
2019-02-09 15:35:15 -0500

    Move to main repository

---
+++ DiceGame/app.js
@@ -0,0 +1,117 @@
+/*
+GAME RULES:
+
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+
+*/
+
+var scores, roundScore, activePlayer, gamePlaying;
+
+init();
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    if (gamePlaying) {
+        // 1. Random number
+        var dice = Math.floor(Math.random() * 6) + 1;
+
+        // 2. Display the result
+        var diceDOM = document.querySelector('.dice')
+        diceDOM.style.display = 'block';
+        diceDOM.src = 'dice-' + dice + '.png';
+        document.getElementById('footer').textContent = 'Player ' + (activePlayer + 1) + ' is' + ' currently' + ' rolling...';
+
+
+        // 3. Update the round score IF the rolled number was NOT a 1.
+        if (dice !== 1) {
+            //Add to score
+            roundScore += dice;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        } else {
+            document.getElementById('footer').textContent = 'Player ' + (activePlayer + 1) + ' rolled a 1, and lost their turn!';
+            // Next Player
+            nextPlayer();
+
+        }
+    }
+});
+
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    if (gamePlaying) {
+        // Add CURRENT score to GLOBAL score
+        scores[activePlayer] += roundScore;
+        document.getElementById('footer').textContent = 'Player ' + (activePlayer + 1) + ' held with ' + roundScore + ' points.';
+
+        // Update the UI
+        document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+
+        // Check if player won the game
+        if (scores[activePlayer] >= 100) {
+            document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+            document.getElementById('footer').textContent = 'Player ' + (activePlayer + 1) + ' has won the game with ' + scores[activePlayer] + ' points!';
+            gamePlaying = false;
+        } else {
+            // Next Player
+            nextPlayer();
+        }
+    }
+});
+
+function nextPlayer() {
+    //Next Player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    //document.querySelector('player-0-panel').classList.add('active');
+    //document.querySelector('player-1-panel').classList.remove('active');
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    document.querySelector('.dice').style.display = 'none';
+}
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
+function init() {
+    scores = [0,0];
+    activePlayer = 0;
+    roundScore = 0;
+    gamePlaying = true;
+
+    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('footer').textContent = 'Time ' + 'to ' + 'play ' + 'a ' + 'game!';
+
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+
+
+}
+
+
+
+
+
+
+//document.querySelector('#current-' + activePlayer).textContent = dice; //The "setter", because it set the value.
+//document.querySelector('#current-' + activePlayer). innerHTML = '<em>' + dice + '</em>';
+//var x = document.querySelector('#score-0').textContent; //The "getter" because it gets a value.
